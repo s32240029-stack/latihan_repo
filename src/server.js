25d43a88c67c577fe4a4f98fa0bf27d711d3c830
s32240029-stack <s32240029@student.ubm.ac.id>
2026-09-21 15:53:18 +0700

    server.js

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ const config = require('./config');
 const { createApp } = require('./app');
 
 // Contoh: node src/server.js --port 8080
+if (!process.env.JWT_SECRET) { 
+  console.error('ERROR: environment variable JWT_SECRET belum diisi.'); 
+  process.exit(1); }
 const args = minimist(process.argv.slice(2));
 const port = args.port || config.port;
 
